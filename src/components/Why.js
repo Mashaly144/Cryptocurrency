1015c23ae39eb5d116711d4e90ad69836d7c9edd
Mashaly144 <117321633+Mashaly144@users.noreply.github.com>
2023-06-15 04:48:47 +0300

    Update Why.js

--- src/components/Why.js
+++ src/components/Why.js
@@ -9,7 +9,7 @@ const Why = () => {
         {/* imag */}
         <div
           data-aos="fade-right"
-          data-aos-offset="400"
+          data-aos-offset="50"
           className="order-1 lg:order-2"
         >
           <img src={whyImag} alt="img" />
@@ -17,7 +17,7 @@ const Why = () => {
         {/* text */}
         <div
           data-aos="fade-left"
-          data-aos-offset="400"
+          data-aos-offset="50"
           className="order-1 max-w-[480px] lg:order-2"
         >
           <h1 className="section-title">Why you should choose CRAPPO</h1>
